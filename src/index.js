bf5b9362bd920775a370a75ce54ea89febc2d5cd
andreaSoncco <andrea.soncco.c@gmail.com>
2023-06-13 15:16:12 -0500

    Septimo commit desde VSC

--- src/index.js
+++ src/index.js
@@ -2,42 +2,46 @@ import validator from './validator.js';
 
 console.log(validator);
 
-let btnBanca = document.getElementById("btnBanca");
+const btnBanca = document.getElementById("btnBanca");
 btnBanca.addEventListener("click", Banca);
 
 function Banca() {
-    document.getElementById("pagina0").style.display = "none";
-    document.getElementById("pagina1").style.display = "block";
- }
-
-
-
-let btnContinuar = document.getElementById("btnContinuar");
-btnContinuar.addEventListener("click", Continuar);
-
-function Continuar() {
-    document.getElementById("pagina1").style.display = "none";
-    document.getElementById("pagina2").style.display = "block";
-    let numeroTarjeta = document.getElementById("numeroTarjeta").value;
-    let numeroInvertido = numeroTarjeta.split("").reverse();
-
-    for (let i = 0; i<numeroInvertido.length; i++){
-        if (i % 2 != 0){
-            numeroInvertido[i]*=2;
-            if (numeroInvertido[i]>=10) {
-                numeroInvertido[i]-=9;
-            }
-                }
-    }
-  
-    let sum = 0;
-    for (let t = 0; t<numeroInvertido.length; t++) {
-        sum += parseInt(numeroInvertido[t]);
-    }
-
-    let resto = sum % 10 == 0;
-    let resultado = (resto) ? "Tu número de tarjeta es válido" : "Tu número de tarjeta no es válido" ;
-    let respuesta = document.getElementById("respuesta");
-    respuesta.innerHTML = resultado;
-    alert(resultado);
+  document.getElementById("pagina0").style.display = "none";
+  document.getElementById("pagina1").style.display = "block";
 }
+
+
+
+const btnContinuar = document.getElementById("btnContinuar");
+btnContinuar.addEventListener("click", validar);
+
+
+function validar() {
+  document.getElementById("pagina1").style.display = "none";
+  document.getElementById("pagina2").style.display = "block";
+  const numeroTarjeta = document.getElementById("numeroTarjeta").value;
+  const resto = validator.isValid(numeroTarjeta)
+  const resultado = (resto) ? "Tu número de tarjeta es válido" : "Tu número de tarjeta no es válido";
+  const respuesta = document.getElementById("respuesta");
+  respuesta.innerHTML = resultado;
+
+  const acumulador = validator.maskify(numeroTarjeta);
+  console.log(acumulador);
+  const respuesta1 = document.getElementById("respuesta1");
+  respuesta1.innerHTML = acumulador;
+}
+
+/*function Maskify(numeroTarjeta) {
+    let acumulador="";
+    for (let i = 0; i < numeroTarjeta.length; i++) {
+        if (i>11) {
+            acumulador = acumulador + numeroTarjeta[i];
+        }
+        else {
+            acumulador = acumulador + "#";
+        }
+    }
+    
+    return acumulador;
+    alert(acumulador);
+} */
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,38 @@
 const validator = {
-  // ...
+  isValid: function (number) {
+    const numeroInvertido = number.split("").reverse();
+
+    for (let i = 0; i < numeroInvertido.length; i++) {
+      if (i % 2 !== 0) {
+        numeroInvertido[i] *= 2;
+        if (numeroInvertido[i] >= 10) {
+          numeroInvertido[i] -= 9;
+        }
+      }
+    }
+
+    let sum = 0;
+    for (let t = 0; t < numeroInvertido.length; t++) {
+      sum += parseInt(numeroInvertido[t]);
+    }
+
+    const resto = sum % 10 === 0;
+    return resto;
+  },
+  maskify: function (number) {
+    let acumulador = "";
+    for (let i = 0; i < number.length; i++) {
+      if (i > 11) {
+        acumulador = acumulador + number[i];
+      }
+      else {
+        acumulador = acumulador + "#";
+      }
+    }
+    return acumulador;
+  }
 };
 
 export default validator;
+
+
