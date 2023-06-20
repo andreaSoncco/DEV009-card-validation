6e9c0bc25f29a672f1f289dd91729986742a3cd5
andreaSoncco <andrea.soncco.c@gmail.com>
2023-06-20 16:23:47 -0500

    Decimooctavo commit desde VSC

--- src/validator.js
+++ src/validator.js
@@ -1,7 +1,9 @@
 const validator = {
   isValid: function (number) {
     const numeroInvertido = number.split("").reverse();
-
+    // 0 1 2 3 4 5 6 7 8 9 10
+    // 2 4 6 7 8 9 2 4 2 1 4
+    let sum = 0;
     for (let i = 0; i < numeroInvertido.length; i++) {
       if (i % 2 !== 0) {
         numeroInvertido[i] *= 2;
@@ -9,21 +11,24 @@ const validator = {
           numeroInvertido[i] -= 9;
         }
       }
+      sum += parseInt(numeroInvertido[i]);
     }
 
-    let sum = 0;
-    for (let t = 0; t < numeroInvertido.length; t++) {
-      sum += parseInt(numeroInvertido[t]);
-    }
 
     const resto = sum % 10 === 0;
-    return resto;
+    if (sum !== 0) {
+      return resto;
+    }
+    else {
+      return false;
+    }
+
   },
   maskify: function (number) {
     let acumulador = "";
     const conteo = parseInt(number.length);
     for (let i = 0; i < number.length; i++) {
-      if (i>(conteo-5)) {
+      if (i > (conteo - 5)) {
         acumulador = acumulador + number[i];
       }
       else {
